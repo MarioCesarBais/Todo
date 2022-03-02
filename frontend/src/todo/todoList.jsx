a7a2bf289d4d1e15f97154ad56c7d04ae1923d78
Mario Cesar Bais <mariocfbais@gmail.com>
2022-03-02 11:51:14 -0300

    tr td ok - construindo exibição lista tarefas

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        return (
+            <tr><td>ok</td></tr>
+        )
+    }
+
+    return (
+    <table className="table">
+        <thead>
+            <tr>
+                <th>Descriçãp</th>
+            </tr>
+        </thead>
+        <tbody>
+            {renderRows()}
+        </tbody>
+    </table>
+    )
+}
